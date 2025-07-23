c5bbf92fd6cfbaa87048947406a67a9faa0ac2cb
Phyo Khant Kyaw <www.pkkcobra01@gmail.com>
2025-07-23 20:49:46 +0700

    added navbar PC size

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <Navbar />
+  );
 }
 
 export default App;
