a863793a5993a4dd54689abb967f9257d7ae38a6
Phyo Khant Kyaw <www.pkkcobra01@gmail.com>
2025-07-28 18:19:08 +0700

    [Phyo Khant Kyaw] Push some part of the main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import "./App.css";
+import MainArchievement from "./components/MainArchievement";
+import MainContext from "./components/MainContext";
+import MainCourses from "./components/MainCourses";
+import MainPartner from "./components/MainPartner";
+import MainSlider from "./components/MainSlider";
 import Navbar from "./components/Navbar";
+import Waves from "react-animated-waves";
 
 function App() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <MainSlider />
+      <MainContext />
+      <MainCourses />
+      <MainArchievement />
+      <MainPartner />
+    </>
   );
 }
 
