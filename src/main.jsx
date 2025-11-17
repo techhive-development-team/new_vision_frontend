2af8a731d57dc1bd91227e57a25d6791f3efd36d
Phyo Khant Kyaw <www.pkkcobra01@gmail.com>
2025-11-17 19:12:22 +0700

    [PKK] Remove handler

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,13 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { DarkModeProvider } from "./context/DarkModeContext";
 import { LoadingProvider } from "./context/LoadingContext.jsx";
-import GlobalErrorHandler from "./context/GlobalErrorHandler.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <DarkModeProvider>
         <LoadingProvider>
-          <GlobalErrorHandler>
             <App />
-          </GlobalErrorHandler>
         </LoadingProvider>
       </DarkModeProvider>
     </BrowserRouter>
